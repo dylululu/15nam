644978aaa9bf37260ea84d0567c87c7b1ffaa061
dylululu <duynhatluu2003@gmail.com>
2023-04-26 16:47:32 +0700

    final?

---
+++ Lib/Control.js
@@ -0,0 +1,14 @@
+  pauseAfterSeconds("G1",2);
+  pauseAfterSeconds("G3",2);
+  audioStartsAt("AM",25);
+  audioStartsAt("bao",3);
+  pauseAfterSeconds("bao",2);
+  audioStartsAt("que1",0.5);
+  // audioStartsAt("quen",0.8);
+  audioStartsAt("que2",0.5);
+  audioStartsAt("que3",0.5);
+
+
+
+
+
